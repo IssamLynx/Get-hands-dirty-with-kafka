0568db4fa6f7e4e573a10b856d24b78366bd52ff
IssamLynx <hm_bakha@esi.dz>
2023-12-25 21:06:13 +0100

    Removed harcodded value for producersConfig

--- src/main/java/com/issamlynx/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/issamlynx/kafka/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.issamlynx.kafka.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,11 +16,13 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapValue;
     // Configuration des propriétés du producteur Kafka
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapValue);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         // Ajoutez d'autres propriétés si nécessaire
